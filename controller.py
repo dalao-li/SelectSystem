8d60cf42e9000273aec2574b54c003346437ca2e
dalao <dalao_li@163.com>
2022-01-15 13:37:48 +0800

    示配手机

--- controller.py
+++ controller.py
@@ -5,7 +5,7 @@ Author: DaLao
 Email: dalao_li@163.com
 Date: 2021-12-31 22:25:47
 LastEditors: DaLao
-LastEditTime: 2022-01-14 03:28:04
+LastEditTime: 2022-01-15 13:37:04
 '''
 
 import random,os,io
